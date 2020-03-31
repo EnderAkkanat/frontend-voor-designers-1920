6a06d5df14245b59f08f5fc2825549c71dce8ffb
EnderAkkanat <ender.akkanat@gmail.com>
2020-03-31 14:04:16 +0200

    OPDRACHT 3 CAROUSEL met JSON
    de afbeeldingen staan in de section geladen. Alleen de start positie klopt nog niet helemaal.

--- opdracht3/script.js
+++ opdracht3/script.js
@@ -1,76 +1,95 @@
-//referentie naar de header en section
-//const header = document.querySelector('header');
-//const section = document.querySelector('section');
+//referentie naar de img en section
+const header = document.querySelector('img');
+const section = document.querySelector('section');
 
 //url json opslaan in een variable om later eruit te halen. 
 let requestURL = 'https://koopreynders.github.io/frontendvoordesigners/opdracht3/json/movies.json';
 
+
 //Request aanmaken dmv object
 let request = new XMLHttpRequest();
-
 //Request openen dmv open()
 request.open('GET', requestURL);
-
 //Response type naar JSON zetten.
 request.responseType = 'json';
 request.send();
 
+
+
+
 //Wachten op response van de server, daarna verwerken.
 request.onload = function() {
-  var allMovies = request.response;
-  console.log(allMovies);
-    var newImage = document.createElement("img");
-    newImage.setAttribute("src", allMovies[3].cover)
-    document.body.appendChild(newImage);
+var allMovies = request.response;
+    console.log(allMovies);
+//maakt img element aan en laad de json erin.
+//zet de img in de section    
+var newImage = document.createElement("img");
+newImage.setAttribute("src", allMovies[0].cover)
+section.appendChild(newImage);
     
-  //titleMovies(allMovies);
+var newImage = document.createElement("img");
+newImage.setAttribute("src", allMovies[1].cover)
+section.appendChild(newImage);
+    
+var newImage = document.createElement("img");
+newImage.setAttribute("src", allMovies[2].cover)
+section.appendChild(newImage);
+    
+var newImage = document.createElement("img");
+newImage.setAttribute("src", allMovies[3].cover)
+section.appendChild(newImage);       
 }
 
-//Object komt van JSON, maak h1, maak textcontent, zet het in Header.
-//function populateHeader(jsonObj) {
-//  const myH1 = document.createElement('h1');
-//  myH1.textContent = jsonObj['squadName'];
-//  header.appendChild(myH1);
-//
-//  const myPara = document.createElement('p');
-//  myPara.textContent = 'Title ' + jsonObj['title'] + ' // releasedate' + jsonObj['release_date'];
-//  header.appendChild(myPara);
-//}
+//carousel code
+var currentfoto = 1;
+var prevfoto = 1; 
 
-//De superhero cards. 
-//Members wordt gestored in een nieuwe variable.
-//Array bevat informatie voor de hero's.
-//function showHeroes(jsonObj) {
-//  const heroes = jsonObj['members'];
-//    
-    //1. Create nieuwe elements  
-//  for (let i = 0; i < heroes.length; i++) {
-//    const myArticle = document.createElement('article');
-//    const myH2 = document.createElement('h2');
-//    const myPara1 = document.createElement('p');
-//    const myPara2 = document.createElement('p');
-//    const myPara3 = document.createElement('p');
-//    const myList = document.createElement('ul');
-//    //2. Secret id voor introduceert informatie.
-//    myH2.textContent = heroes[i].name;
-//    myPara1.textContent = ' ' + heroes[i].secretIdentity;
-//    myPara2.textContent = 'Age: ' + heroes[i].age;
-//    myPara3.textContent = 'Superpowers:';
-//    //3. Sla de powers property op.
-//    //4. Nieuwe for loop om door de loop te gaan.  
-//    const superPowers = heroes[i].powers;
-//    for (let j = 0; j < superPowers.length; j++) {
-//      const listItem = document.createElement('li');
-//      listItem.textContent = superPowers[j];
-//      myList.appendChild(listItem);
-//    }
-//
-//    myArticle.appendChild(myH2);
-//    myArticle.appendChild(myPara1);
-//    myArticle.appendChild(myPara2);
-//    myArticle.appendChild(myPara3);
-//    myArticle.appendChild(myList);
-//
-//    section.appendChild(myArticle);
-//    }
-//}
+//Proberen te navigeren met de arrows dmv keydown event.
+document.getElementsByID("prev").addEventListener("keydown",prev);
+//Voer vorige function uit.
+function prev(){
+    vorige();
+}
+document.getElementsByID("next").addEventListener("keydown",next);
+//Voer volgende function uit.
+function next(){
+    volgende();
+}
+
+function volgende() {
+    
+    currentfoto = currentfoto+1;
+    if (currentfoto >4){
+        currentfoto =1;
+    }
+    console.log(currentfoto)
+    
+    beweeg();
+
+}
+function vorige() {
+    currentfoto = currentfoto-1;
+    if (currentfoto <1){
+        currentfoto=4;
+    }
+    console.log(currentfoto)
+
+    beweeg();
+}
+
+function beweeg(){
+    console.log("beweeg")
+    if(currentfoto == 1){
+            document.getElementById("container").style.marginLeft = "0px";
+    }
+    
+    if(currentfoto == 2){
+            document.getElementById("container").style.marginLeft = "-600px";
+    }1
+    if(currentfoto == 3){
+            document.getElementById("container").style.marginLeft = "-1200px";
+    }
+    if(currentfoto == 4){
+            document.getElementById("container").style.marginLeft = "-1800px";
+    }
+}
