85a906a59f96a5b18992a0a15d55327a3d558b29
EnderAkkanat <ender.akkanat@gmail.com>
2020-04-14 14:12:19 +0200

    Feedback verwerkt

--- opdracht3/script.js
+++ opdracht3/script.js
@@ -8,93 +8,91 @@ let requestURL = 'https://koopreynders.github.io/frontendvoordesigners/opdracht3
 
 //Request aanmaken dmv object
 let request = new XMLHttpRequest();
-//Request openen dmv open()
-request.open('GET', requestURL);
+
 //Response type naar JSON zetten.
 request.responseType = 'json';
-request.send();
-
-
 
 
 //Wachten op response van de server, daarna verwerken.
 request.onload = function() {
 var allMovies = request.response;
     console.log(allMovies);
-//maakt img element aan en laad de json erin.
-//zet de img in de section    
-var newImage = document.createElement("img");
-newImage.setAttribute("src", allMovies[0].cover)
-section.appendChild(newImage);
-    
-var newImage = document.createElement("img");
-newImage.setAttribute("src", allMovies[1].cover)
-section.appendChild(newImage);
-    
-var newImage = document.createElement("img");
-newImage.setAttribute("src", allMovies[2].cover)
-section.appendChild(newImage);
-    
-var newImage = document.createElement("img");
-newImage.setAttribute("src", allMovies[3].cover)
-section.appendChild(newImage);    
-    
-var newImage = document.createElement("img");
-newImage.setAttribute("src", allMovies[4].cover)
-section.appendChild(newImage);   
 
-var newImage = document.createElement("img");
-newImage.setAttribute("src", allMovies[5].cover)
-section.appendChild(newImage);   
+    
+//Een for loop om door de array heen te lopen voor de images. 
+for (var item of allMovies) {
+        //maak een article element om alle film dat in te zetten
+        var article = document.createElement('article');
+        //maak een image aan
+        var newImage = document.createElement('img');
+        //haal de url uit de json data
+        newImage.setAttribute("src", item.cover)
+        //koppel de image aan het article
+        article.appendChild(newImage);
+
+        //koppel de <article> aan de section op de pagina
+        section.appendChild(article);
+      };
 
 }
+//Request openen dmv open()
+request.open('GET', requestURL);
+request.send();
 
-//carousel code
+//carousel code, geef de waarde van de foto aan.
 var currentfoto = 1;
 var prevfoto = 1; 
 
-//Proberen te navigeren met de arrows dmv keydown event.
-document.getElementsByID("prev").addEventListener("keydown",prev);
-//Voer vorige function uit.
-function prev(){
-    vorige();
-}
-document.getElementsByID("next").addEventListener("keydown",next);
-//Voer volgende function uit.
-function next(){
+document.addEventListener('keydown', logKey);
+
+//functie voor pijltjes aanmaken
+function logKey(e) {
+  console.log("key",e.key);
+  console.log("keyCode",e.keyCode);
+  //Als e.key arrowright is voer dan de functie volgende() uit. 
+  if(e.key == "ArrowRight"){
     volgende();
+  }
+  //Als e.key arrowleft is voer dan de functie vorige() uit.     
+  if (e.key =="ArrowLeft"){
+    vorige();  
+  }     
 }
 
+
 function volgende() {
-    
+//Code voor de teller reset.    
     currentfoto = currentfoto+1;
+//Als de waarde van de foto boven de 4 is, reset de foto waarde naar 1.     
     if (currentfoto >4){
         currentfoto =1;
     }
     console.log(currentfoto)
-    
+//Voer functie beweeg uit.    
     beweeg();
 
 }
 function vorige() {
     currentfoto = currentfoto-1;
+//Als de waarde van de foto onder de 1 is, reset de foto waarde naar 4.      
     if (currentfoto <1){
         currentfoto=4;
     }
     console.log(currentfoto)
-
+//Voer functie beweeg uit.
     beweeg();
 }
 
 function beweeg(){
     console.log("beweeg")
+ 
     if(currentfoto == 1){
             document.getElementById("container").style.marginLeft = "0px";
     }
-    
+ //Als de waarde van de foto 2 is, schuif de container dan op naar links met 600px.       
     if(currentfoto == 2){
             document.getElementById("container").style.marginLeft = "-600px";
-    }1
+    }
     if(currentfoto == 3){
             document.getElementById("container").style.marginLeft = "-1200px";
     }
